62220a567a8c84726be662b55b19bc38c431c6e1
“mchae” <mchae@student.42seoul.kr>
2020-10-22 23:17:14 +0900

    war_machine_clear

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,17 +6,11 @@
 /*   By: mchae <mchae@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/18 20:25:23 by mchae             #+#    #+#             */
-/*   Updated: 2020/10/22 21:27:39 by mchae            ###   ########.fr       */
+/*   Updated: 2020/10/22 23:07:26 by mchae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static void		passing_delimiter(const char **s, char c)
-{
-	while (**s == c && **s)
-		*s += 1;
-}
 
 static size_t	separation_num(const char *s, char c)
 {
@@ -34,7 +28,7 @@ static size_t	separation_num(const char *s, char c)
 			num++;
 		}
 		else
-			passing_delimiter(&s, c);
+			s++;
 	}
 	return (num);
 }
@@ -44,20 +38,20 @@ static size_t	get_alloc_size(char const *s, char c)
 	size_t i;
 
 	i = 0;
-	while (s[i] != c)
+	while (s[i] && s[i] != c)
 		i++;
 	return (i);
 }
 
-static void		*free_arr(char **double_str, size_t arr_size, size_t index)
+static void		*free_arr(char **double_str, size_t index)
 {
-	while (arr_size-- >= 0)
+	size_t	i;
+
+	i = 0;
+	while (i < index)
 	{
-		while (index >= 0)
-		{
-			free(double_str[index]);
-			double_str[index--] = 0;
-		}
+		free(double_str[index]);
+		double_str[i++] = 0;
 	}
 	free(double_str);
 	double_str = 0;
@@ -75,16 +69,17 @@ char			**ft_split(char const *s, char c)
 		return (0);
 	index = 0;
 	arr_size = separation_num(s, c);
-	if (!(double_str = (char**)malloc(sizeof(char*) * arr_size)))
+	if (!(double_str = (char**)malloc(sizeof(char*) * (arr_size + 1))))
 		return (NULL);
 	while (arr_size--)
 	{
-		passing_delimiter(&s, c);
-		alloc_size = get_alloc_size(s, c);
-		if (!(double_str[index] = (char*)malloc(alloc_size + 1)))
-			return (free_arr(double_str, arr_size, index));
-		ft_strlcpy(double_str[index++], s, alloc_size + 1);
-		s += alloc_size;
+		while (*s && *s == c)
+			s++;
+		alloc_size = get_alloc_size(s, c) + 1;
+		if (!(double_str[index] = (char*)malloc(alloc_size)))
+			return (free_arr(double_str, index));
+		ft_strlcpy(double_str[index++], s, alloc_size);
+		s = ft_strchr(s, c);
 	}
 	double_str[index] = 0;
 	return (double_str);
--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mchae <mchae@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/14 18:25:45 by mchae             #+#    #+#             */
-/*   Updated: 2020/10/14 23:50:17 by mchae            ###   ########.fr       */
+/*   Updated: 2020/10/22 22:15:04 by mchae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!s || ft_strlen(s) <= start || !len)
 		return (ft_strdup(""));
-	if (!(str = malloc(++len)))
+	if (!(str = malloc(len + 1)))
 		return (0);
-	ft_strlcpy(str, s + start, len);
+	ft_strlcpy(str, s + start, len + 1);
 	return (str);
 }
