ed055382e04e15e0689c85c7ee0d3e9f27524c88
채명석 <chaemyeongseog@chaemyeongseog-ui-MacBookPro.local>
2021-01-18 15:51:01 +0900

    move_x_y 1

--- 5_cub3d/minilibx_opengl_20191021/my_page/util.c
+++ 5_cub3d/minilibx_opengl_20191021/my_page/util.c
@@ -7,10 +7,34 @@ void	redraw(t_game *game)
 
 void	move_x(t_game *game, int dir)
 {
-
+	if (game->char_mask_x + dir > 0 && game->char_mask_x + dir < TILE_SIZE - 1)
+		game->char_mask_x += dir;
+	else
+	{
+		if (game->char_x + dir != 1)
+		{
+			game->char_x += dir;
+			if (dir > 0)
+				game->char_mask_x = 0;
+			else
+				game->char_mask_x = TILE_SIZE - 1;
+		}
+	}
 }
 
 void	move_y(t_game *game, int dir)
 {
-
+	if (game->char_mask_y + dir > 0 && game->char_mask_y + dir < TILE_SIZE - 1)
+		game->char_mask_y += dir;
+	else
+	{
+		if (game->char_y + dir != 1)
+		{
+			game->char_y += dir;
+			if (dir > 0)
+				game->char_mask_y = 0;
+			else
+				game->char_mask_y = TILE_SIZE - 1;
+		}
+	}
 }
