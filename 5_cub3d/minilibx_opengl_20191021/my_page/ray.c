5b328ad9518d4e493f0e17b88e28fbf59500a06c
채명석 <chaemyeongseog@chaemyeongseog-ui-MacBookPro.local>
2021-01-21 15:47:15 +0900

    ray temp_2

--- 5_cub3d/minilibx_opengl_20191021/my_page/ray.c
+++ 5_cub3d/minilibx_opengl_20191021/my_page/ray.c
@@ -2,8 +2,8 @@
 
 void	ray_init(t_game *game)
 {
-	game->ray.char_pos_x = (double)(game->char_x * TILE_SIZE);
-	game->ray.char_pos_y = (double)(game->char_y * TILE_SIZE);
+	game->ray.char_pos_x = (double)((game->char_x + 1) * game->char_mask_x);
+	game->ray.char_pos_y = (double)((game->char_y + 1) * game->char_mask_y);
 	game->ray.dir_x = -1;
 	game->ray.dir_y = 0;
 	game->ray.plane_x = 0;
@@ -60,10 +60,10 @@ void	raycast(t_game *game)
 				game->ray.map_y += game->ray.step_y;
 				game->ray.side = 1;
 			}
-			if (game->img.data[game->ray.map_y * game->ray.map_x] == 0xFFFFFF)
+			if (game->img.data[(game->ray.map_y + game->char_mask_y) * WIDTH + (game->ray.map_x + game->char_mask_x)] == 0xFFFFFF)
 				game->ray.hit = 1;
 			else
-				game->img.data[game->ray.map_y * game->ray.map_x] = 0xFF00FF;
+				game->img.data[(game->ray.map_y + game->char_mask_y) * WIDTH + (game->ray.map_x + game->char_mask_x)] = 0x00FF00;
 		}
 	}
 }
