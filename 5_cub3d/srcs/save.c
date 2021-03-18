b102f9fc7e72aa45889965657bd0540a16e89c82
“mchae” <mchae@student.42seoul.kr>
2021-03-19 03:32:23 +0900

    save ok

--- 5_cub3d/srcs/save.c
+++ 5_cub3d/srcs/save.c
@@ -6,7 +6,7 @@
 /*   By: mchae <mchae@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 16:02:14 by mchae             #+#    #+#             */
-/*   Updated: 2021/03/19 03:17:45 by mchae            ###   ########.fr       */
+/*   Updated: 2021/03/19 03:29:31 by mchae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,11 +34,11 @@ static int		bit_header_set(int fd, t_game *game)
 	ft_bzero(bmp_header, 54);
 	bmp_header[0] = 'B';
 	bmp_header[1] = 'M';
-	int_to_char(54 + (game->info.screen_width + game->img.padding) \
+	int_to_char(54 + 3 * (game->info.screen_width) \
 		* game->info.screen_height, bmp_header + 2);
 	bmp_header[10] = (unsigned char)54;
 	bmp_header[14] = (unsigned char)40;
-	int_to_char(game->info.screen_width + game->img.padding, bmp_header + 18);
+	int_to_char(game->info.screen_width, bmp_header + 18);
 	int_to_char(game->info.screen_height, bmp_header + 22);
 	bmp_header[26] = (unsigned char)1;
 	bmp_header[28] = (unsigned char)24;
@@ -60,7 +60,7 @@ static void		write_bmp_data_pixel(int fd, t_game *game)
 		j = -1;
 		while (++j < game->info.screen_width)
 		{
-			color = game->img.data[(game->info.screen_width + game->img.padding) * i + j];
+			color = game->img.data[(game->info.screen_width) * i + j];
 			if (write(fd, &color, 3) < 0)
 				error_exit("color write error");
 		}
@@ -69,8 +69,11 @@ static void		write_bmp_data_pixel(int fd, t_game *game)
 
 void			save_bmp(t_game *game)
 {
-	int					fd;
+	int		fd;
+	int		pixel_bytes_per_row;
 
+	pixel_bytes_per_row = game->info.screen_width * 3;
+	game->img.padding = (4 - pixel_bytes_per_row % 4) % 4;
 	fd = open("screenshot.bmp", O_RDWR | O_CREAT, 0600);
 	if (fd < 0)
 		error_exit("bmp open error");
