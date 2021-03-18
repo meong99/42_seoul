835980123d3a3ebbd029586feedc1d6645c7151c
“mchae” <mchae@student.42seoul.kr>
2021-03-18 13:47:17 +0900

    sprite sort

--- 5_cub3d/srcs/sort.c
+++ 5_cub3d/srcs/sort.c
@@ -6,19 +6,19 @@
 /*   By: mchae <mchae@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/14 16:16:16 by mchae             #+#    #+#             */
-/*   Updated: 2021/03/18 13:44:23 by mchae            ###   ########.fr       */
+/*   Updated: 2021/03/18 13:45:59 by mchae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	swap_value(t_sprite_dist **value, int index_1, int index_2)
+void	swap_value(t_sprite_dist *value, int index_1, int index_2)
 {
 	t_sprite_dist	temp;
 
-	temp = (*value)[index_1];
-	(*value)[index_1] = (*value)[index_2];
-	(*value)[index_2] = temp;
+	temp = value[index_1];
+	value[index_1] = value[index_2];
+	value[index_2] = temp;
 }
 
 int		partition(void *value, int left_index, int right_index)
@@ -35,10 +35,10 @@ int		partition(void *value, int left_index, int right_index)
 		if (sprite_dist[j].dist > sprite_dist[right_index].dist)
 		{
 			i++;
-			swap_value(&sprite_dist, i, j);
+			swap_value(sprite_dist, i, j);
 		}
 	}
-	swap_value(&sprite_dist, i + 1, right_index);
+	swap_value(sprite_dist, i + 1, right_index);
 	return (i + 1);
 }
 
