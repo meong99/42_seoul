f2f643a9e0d1e1c6d76c2fb6c7a89dacdbd96d14
mchae <mchae@student.42seoul.kr>
2021-07-17 17:41:17 +0900

    main mchae

--- 10_libasm/srcs/main.c
+++ 10_libasm/srcs/main.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mchae <mchae@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/11 18:09:57 by sohelee           #+#    #+#             */
-/*   Updated: 2021/07/09 17:13:51 by mchae            ###   ########.fr       */
+/*   Created: 2021/07/17 17:41:05 by mchae             #+#    #+#             */
+/*   Updated: 2021/07/17 17:41:06 by mchae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	write_test(void)
 {
-	printf("[unistd] write = %zd\n", write(1, "sohelee!", 9));
-	printf("[libasm] ft_write = %zd\n", ft_write(1, "sohelee!", 9));
+	printf("[unistd] write = %zd\n", write(1, "mchae!", 9));
+	printf("[libasm] ft_write = %zd\n", ft_write(1, "mchae!", 9));
 	printf("\n");
 }
 
@@ -50,11 +50,11 @@ int		main(void)
 	char s[50];
 
 	printf("______TESTING <ft_strlen>______\n");
-	printf("[string] strlen = %zu\n", strlen("sohelee!"));
-	printf("[libasm] ft_strlen = %zu\n\n", ft_strlen("sohelee!"));
+	printf("[string] strlen = %zu\n", strlen("mchae!"));
+	printf("[libasm] ft_strlen = %zu\n\n", ft_strlen("mchae!"));
 	printf("______TESTING <ft_strcpy>______\n");
-	printf("[string] strcpy = %s\n", strcpy(s, "sohelee!"));
-	printf("[libasm] ft_strcpy = %s\n\n", ft_strcpy(s, "sohelee!"));
+	printf("[string] strcpy = %s\n", strcpy(s, "mchae!"));
+	printf("[libasm] ft_strcpy = %s\n\n", ft_strcpy(s, "mchae!"));
 	printf("______TESTING <ft_strcmp>______\n");
 	printf("[string] strcmp = %d\n", strcmp("abcde", "abcd"));
 	printf("[libasm] ft_strcmp = %d\n\n", ft_strcmp("abcde", "abcd"));
