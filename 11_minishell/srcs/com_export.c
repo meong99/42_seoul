6e5d05a95b0e1caef2b2f18e888c2c943b1c8e1a
mchae <mchae@student.42seoul.kr>
2021-09-29 21:10:15 +0900

    env parsing done

--- 11_minishell/srcs/com_export.c
+++ 11_minishell/srcs/com_export.c
@@ -2,25 +2,25 @@
 
 static t_env	*find_key(t_env *env, char *key)
 {
-	t_env	*index;
+	t_env	*node;
 
-	index = env;
-	while (index)
+	node = env;
+	while (node)
 	{
-		if (ft_strncmp(index->key, key, ft_strlen(key)) == 0)
-			return (index);
-		index = index->next;
+		if (ft_strncmp(node->key, key, ft_strlen(key)) == 0)
+			return (node);
+		node = node->next;
 	}
 	return (NULL);
 }
 
 static void	mapping_value(t_env *env, char *key, char *value)
 {
-	t_env	*temp;
+	t_env	*node;
 
-	temp = find_key(env, key);
-	free(temp->key);
-	temp->key = value;
+	node = find_key(env, key);
+	free(node->key);
+	node->key = value;
 }
 
 static void	ft_putenv(t_env *env, char *key, char *value)
@@ -45,17 +45,21 @@ void	exe_export(t_commands *commands, t_env *env)
 {
 	char	*str;
 	char	**split_var;
-	t_list	*index;
+	t_list	*node;
 
-	index = commands->arg;
-	while (index)
+	node = commands->arg;
+	if (node == NULL)
 	{
-		split_var = ft_split((char*)index->content, '=');
+		print_env(env);
+	}
+	while (node)
+	{
+		split_var = ft_split((char*)node->content, '=');
 		str = getenv(split_var[0]);
 		if (str == NULL)
 			ft_putenv(env, split_var[0], split_var[1]);
 		else
 			mapping_value(env, split_var[0], split_var[1]);
-		index = index->next;
+		node = node->next;
 	}
 }
--- 11_minishell/srcs/init_all.c
+++ 11_minishell/srcs/init_all.c
@@ -16,12 +16,32 @@ static t_env	*new_node(char *key, char *value)
 	return (node);
 }
 
+static void		append_node(t_env *env, char **envp)
+{
+	t_env	*node;
+	char	**spl_envp;
+
+	while (*envp)
+	{
+		spl_envp = ft_split(*envp, '=');
+		node = new_node(spl_envp[0], spl_envp[1]);
+		env->next = node;
+		env = env->next;
+		envp++;
+	}
+}
+
 t_env			*init_env(char **envp)
 {
 	t_env	*env;
-	t_env	*node;
-	
+	char	**spl_envp;
 
+	spl_envp = ft_split(*envp, '=');
+	env = new_node(spl_envp[0], spl_envp[1]);
+	envp++;
+	ft_free(spl_envp, 0, true);
+	append_node(env, envp);
+	return (env);
 }
 
 void			init_commands(t_commands *commands, t_env *env)
