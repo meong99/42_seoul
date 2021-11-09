140d329bfd84209ecad85ee4114ebe78999d2273
meong99 <mchae@student.42seoul.kr>
2021-11-10 05:19:52 +0900

    if no value of env there's not put

--- 11_minishell/srcs/command/com_env.c
+++ 11_minishell/srcs/command/com_env.c
@@ -6,7 +6,7 @@
 /*   By: mchae <mchae@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 23:45:49 by mchae             #+#    #+#             */
-/*   Updated: 2021/11/08 23:45:49 by mchae            ###   ########.fr       */
+/*   Updated: 2021/11/10 05:13:33 by mchae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,11 @@ void	exe_env(t_commands *commands)
 	temp = g_commands->env;
 	while (temp)
 	{
+		printf("%s=", temp->key);
 		if (temp->value)
-			printf("%s=%s\n", temp->key, temp->value);
+			printf("%s\n", temp->value);
+		else
+			printf("\n");
 		temp = temp->next;
 	}
 	commands = 0;
