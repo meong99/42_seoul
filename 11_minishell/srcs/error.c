be2e1518101b79d7af3e4769bd33b9d495c38484
meong99 <mchae@student.42seoul.kr>
2021-11-19 23:49:27 +0900

    renaming of err*.c

--- 11_minishell/srcs/error.c
+++ 11_minishell/srcs/error.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error.c                                            :+:      :+:    :+:   */
+/*   error2.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mchae <mchae@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/08 23:47:20 by mchae             #+#    #+#             */
-/*   Updated: 2021/11/19 23:42:37 by mchae            ###   ########.fr       */
+/*   Created: 2021/11/19 23:22:39 by mchae             #+#    #+#             */
+/*   Updated: 2021/11/19 23:47:18 by mchae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,59 +52,40 @@ int	closed_quote(char *str)
 	return (result);
 }
 
-static int	check_after_pipe(char *str)
+char	*join_errmsg(char *str1, char *str2, char *str3, char *str4)
 {
-	char	*tmp;
+	char	*str;
 
-	tmp = str;
-	while (1)
+	str = ft_strdup("minishell: ");
+	if (str1)
+		str = ft_strjoin_free(str, str1);
+	if (str2)
 	{
-		tmp = ft_strchr_f(tmp, '|', BOTH, check_quote);
-		if (tmp == 0)
-			break ;
-		tmp++;
-		while (*tmp == ' ')
-			tmp++;
-		if (*tmp == '|' || *tmp == '\0')
-		{
-			free(str);
-			print_systax_err("|");
-			return (1);
-		}
+		str = ft_strjoin_free(str, ": ");
+		str = ft_strjoin_free(str, str2);
 	}
-	return (0);
+	if (str3)
+	{
+		str = ft_strjoin_free(str, ": ");
+		str = ft_strjoin_free(str, str3);
+	}
+	if (str4)
+	{
+		str = ft_strjoin_free(str, ": ");
+		str = ft_strjoin_free(str, str4);
+	}
+	str = ft_strjoin_free(str, "\n");
+	return (str);
 }
 
-static int	check_pipe_err(char *str)
+void	put_err(char *source_err, int use_exit)
 {
-	int		i;
+	char	*errstr;
 
-	i = 0;
-	while (str[i] == ' ')
-		i++;
-	if (str[i] == '|')
-	{
-		free(str);
-		print_systax_err("|");
-		return (1);
-	}
-	return (check_after_pipe(str));
-}
-
-int	check_str_err(char *str)
-{
-	if (str == NULL)
-	{
-		ft_putstr_fd("\x1b[1A\033[11Cexit\n", STDOUT_FILENO);
-		tcsetattr(STDIN_FILENO, TCSANOW, &g_commands->oldterm);
-		exit(0);
-	}
-	if (*str == 0 || !closed_quote(str))
-	{
-		free(str);
-		return (1);
-	}
-	if (*str)
-		add_history(str);
-	return (check_pipe_err(str));
+	errstr = strerror(errno);
+	errstr = join_errmsg(source_err, errstr, 0, 0);
+	ft_putstr_fd(errstr, STDERR_FILENO);
+	free(errstr);
+	if (use_exit)
+		exit(errno);
 }
