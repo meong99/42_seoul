867d9cb2f24f326cd41b76afb8afd8b94b92a35d
meong99 <mchae@student.42seoul.kr>
2021-12-09 06:16:54 +0900

    cpp01.ex02 Hi this is brain~

---
+++ 12_cpp/2_cpp01/ex02/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int	main( void )
+{
+	std::string	string = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &string;
+	std::string &stringREF = *stringPTR;
+
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+	
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+}
