37509b5f0fc33390c1f026ef0b4a1e242659c761
meong99 <mchae@student.42seoul.kr>
2021-12-11 22:12:42 +0900

    cpp01.ex05 karen2.0

--- 12_cpp/2_cpp01/ex05/Karen.hpp
+++ 12_cpp/2_cpp01/ex05/Karen.hpp
@@ -6,10 +6,10 @@
 class Karen
 {
 private:
-	void debug( void );
-	void info( void );
-	void warning( void );
-	void error( void );
+	void	debug( void );
+	void	info( void );
+	void	warning( void );
+	void	error( void );
 
 public:
 	Karen(void);
--- 12_cpp/2_cpp01/ex05/main.cpp
+++ 12_cpp/2_cpp01/ex05/main.cpp
@@ -2,5 +2,21 @@
 
 int	main(void)
 {
+	Karen karen;
 
+	std::cout << "DEBUG" << std::endl;
+	karen.complain("DEBUG");
+	std::cout << std::endl;
+
+	std::cout << "INFO" << std::endl;
+	karen.complain("INFO");
+	std::cout << std::endl;
+
+	std::cout << "WARNING" << std::endl;
+	karen.complain("WARNING");
+	std::cout << std::endl;
+
+	std::cout << "ERROR" << std::endl;
+	karen.complain("ERROR");
+	std::cout << std::endl;
 }
