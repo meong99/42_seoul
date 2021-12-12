3ebac522f95e19d4f2b03418af4a19290c25248c
meong99 <mchae@student.42seoul.kr>
2021-12-12 23:29:50 +0900

    make dir cpp02

---
+++ 12_cpp/2_cpp02/ex00/Fixed.hpp
@@ -0,0 +1,23 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+private:
+	
+public:
+	Fixed(void);
+	~Fixed(void);
+};
+
+Fixed::Fixed(void)
+{
+}
+
+Fixed::~Fixed(void)
+{
+}
+
+#endif
