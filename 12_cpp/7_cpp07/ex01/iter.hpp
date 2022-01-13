7a2186688ff5ab55b36ad7e9ec1a41fb2f6cc6b2
meong99 <mchae@student.42seoul.kr>
2022-01-13 19:11:45 +0900

    cpp07.01 finished

--- 12_cpp/7_cpp07/ex01/iter.hpp
+++ 12_cpp/7_cpp07/ex01/iter.hpp
@@ -1,4 +1,13 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
+template <typename T>
+void iter(const T *arr, int len, void(*f)(const T &))
+{
+	for (int i = 0; i < len; i++)
+	{
+		f(arr[i]);
+	}
+}
+
 #endif
--- 12_cpp/7_cpp07/ex01/main.cpp
+++ 12_cpp/7_cpp07/ex01/main.cpp
@@ -1,7 +1,19 @@
-#include "iter.hpp"
 #include <iostream>
+#include "iter.hpp"
+
+template <typename T>
+void	ft_print(const T &element)
+{
+	std::cout << element << std::endl;
+}
 
 int	main(void)
 {
-	
+	std::string s_arr[5] = {"hello", "42", "seoul", "mchae", "world"};
+	int			i_arr[6] = {1, 2, 3, 4, 5, 6};
+
+	std::cout << "------string------" << std::endl;
+	iter(s_arr, 5, ft_print);
+	std::cout << "------int------" << std::endl;
+	iter(i_arr, 6, ft_print);
 }
